98a2756d07c57738a788f5666ba6d0c6e7c1c9e7
StratiX0 <115560299+StratiX0@users.noreply.github.com>
2025-03-31 12:31:31 +0200

    Add BuildData ScriptableObject

---
+++ Assets/Script/Other/Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Utiles utiles;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        utiles.PrintBuildingData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
