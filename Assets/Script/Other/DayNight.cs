a013d1db009a4b724f42bf4e422a0047d443c0a1
StratiX0 <115560299+StratiX0@users.noreply.github.com>
2025-04-02 17:24:30 +0200

    ADD Button to change time
    ADD:
    - Button to change the time,
    
    UPDATE:
    - Time cannot be changed from inspector anymore, forced to click on button

--- Assets/Script/Other/DayNight.cs
+++ Assets/Script/Other/DayNight.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using LitMotion;
 using LitMotion.Extensions;
+using TMPro;
 using UnityEngine.UI;
 
 public class DayNight : MonoBehaviour
 {
     [SerializeField] private bool isDay;
+    
+    [SerializeField] private TMP_Text timeText;
     
     [Header("Light Parameters")]
     [SerializeField] private Light sun;
@@ -28,29 +31,19 @@ public class DayNight : MonoBehaviour
     [SerializeField] private Vector3 shownPosition;
     
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         sun.color = isDay ? dayColor : nightColor;
         sun.transform.rotation = Quaternion.Euler(isDay ? dayRotation : nightRotation);
         RenderSettings.skybox = isDay ? daySkybox : nightSkybox;
     }
-
-    private void OnValidate()
+    
+    public void ChangeTime()
     {
-        var closeCurtain = LMotion.Create(curtain.transform.position.x, shownPosition.x, animationDuration)
+        isDay = !isDay;
+        LMotion.Create(curtain.transform.position.x, shownPosition.x, animationDuration)
             .WithEase(Ease.OutCubic).WithOnComplete(SwitchTime)
             .BindToPositionX(curtain.transform);
-        
-
-        
-        // var openCurtain = LMotion.Create(curtain.transform.position.x, hiddenPosition.x, animationDuration)
-        //     .WithEase(Ease.InOutBack)
-        //     .BindToPositionX(curtain.transform);
-        //
-        // if (openCurtain.IsActive())
-        // {
-        //     openCurtain.Complete();
-        // }
     }
 
     private void SwitchTime()
@@ -59,8 +52,10 @@ public class DayNight : MonoBehaviour
         RenderSettings.skybox = isDay ? daySkybox : nightSkybox;
         sun.transform.rotation = Quaternion.Euler(isDay ? dayRotation : nightRotation);
         
-        var openCurtain = LMotion.Create(curtain.transform.position.x, hiddenPosition.x, animationDuration)
+        LMotion.Create(curtain.transform.position.x, hiddenPosition.x, animationDuration)
             .WithEase(Ease.OutCubic)
             .BindToPositionX(curtain.transform);
+        
+        timeText.text = isDay ? "Day" : "Night";
     }
 }
