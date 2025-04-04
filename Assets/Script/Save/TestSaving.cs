d23f46a97c032bfdbeea15c30b1722065faa1a01
unknown <ycaillot@gamingcampus.local>
2025-04-04 10:59:21 +0200

    UPDATE FINISH save data Save Directory : .\AppData\LocalLow\DefaultCompany\DreamVillage\Saves New : 	- Data saving on a file for every classes 	- Data saving via interface to ensure it's efficacity and 	  usability 	- Encrytation of files 	- directory and file creation 	- TestSaving class to be an exemple of the save

--- Assets/Script/Save/TestSaving.cs
+++ Assets/Script/Save/TestSaving.cs
@@ -1,66 +1,53 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class TestSaving : MonoBehaviour
+public class TestSaving : MonoBehaviour, ISaveable<TestSaving.SavePartData>
 {
-    string name = "Player";
-    float floatifloata = 0.5f;
-    int intotiti = 45;
-    bool boolabool = true;
-    List<int> listou = new List<int> { 1, 2, 3, 4, 5 };
-    Dictionary<string, InhabitantEnums> dictou = new Dictionary<string, InhabitantEnums>
+    [System.Serializable]
+    public class SavePartData : ISaveData
     {
-        { "Key1", InhabitantEnums.INTJ },
-        { "Key2", InhabitantEnums.INFP},
-        { "Key3", InhabitantEnums.ENTP}
-    };
+        public string name = "Player";
+        float floatifloata = 0.5f;
+        int intotiti = 456;
+        bool boolabool = true;
+        List<int> listou = new List<int> { 1, 2, 3, 4, 5 };
+        Dictionary<string, InhabitantEnums> dictou = new Dictionary<string, InhabitantEnums>
+        {
+            { "Key1", InhabitantEnums.INTJ },
+            { "Key2", InhabitantEnums.INFP},
+            { "Key3", InhabitantEnums.ENTP}
+        };
+    }
 
+    private string name;
 
     private void Awake()
     {
-
-        TestSaving testSaving = new TestSaving();
-        string save = JsonUtility.ToJson(testSaving);
-
-        TestSaving testSaving2 = JsonUtility.FromJson<TestSaving>(save);
-
-        Debug.Log(Application.persistentDataPath + "/save.dat");
-        if (!File.Exists(Application.persistentDataPath + "/save.dat"))
-        {
-            //Create file
-            File.Create(Application.persistentDataPath + "/save.dat").Close();
-        }
-
-        // Save data
-        Dictionary<string, object> data = new Dictionary<string, object>
-        {
-            { "name", name },
-            { "floatifloata", floatifloata },
-            { "intotiti", intotiti },
-            { "boolabool", boolabool },
-            { "listou", listou },
-            { "dictou", dictou }
-        };
-
-        PlayerSave.SaveData("PlayerData", data);
-        //PlayerSave.SaveFile();
+        /*SaveDataManager.saveData.testSaving = this;
+        SaveDataManager.Save();*/
     }
-
 
 
     private void Start()
     {
-        // Load data
-        Dictionary<string, object> loadedData = PlayerSave.LoadData("PlayerData");
-        // Display loaded data
+        name = "PlayerOnStart";
 
+        this.Save("TestSaving");
+        this.Load("TestSaving");
 
-        Debug.Log("Loaded Data:");
-        foreach (var item in loadedData)
-        {
-            Debug.Log($"{item.Key}: {item.Value}");
-        }
+    }
+
+    public SavePartData Serialize()
+    {
+        SavePartData data = new SavePartData();
+        data.name = name;
+
+        return data;
+    }
+
+    public void Deserialize(SavePartData data)
+    {
+        name = data.name;
     }
 }
