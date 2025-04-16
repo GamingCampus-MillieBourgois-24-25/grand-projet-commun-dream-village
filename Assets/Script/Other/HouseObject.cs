ef0306f6cc8e8f9eea788b4d0b3dcc4cfd927a3c
aiskal <icingdawn@gmail.com>
2025-04-16 14:37:21 +0200

    ADD Ui elements

---
+++ Assets/Script/Other/HouseObject.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class HouseObject : MonoBehaviour
+{
+    [SerializeField] private Inhabitant inhabitant;
+    [SerializeField] private GameObject canvasHabitant;
+    private Transform spawnPoint;
+
+    private GameObject instantiatedPrefab;
+
+    private void Start()
+    {
+        spawnPoint = transform.Find("SpawnPoint");
+    }
+
+    private void OnMouseDown()
+    {
+        if (!GM.IM.isEditMode)
+        {
+            if (canvasHabitant != null && !canvasHabitant.activeSelf)
+                canvasHabitant.SetActive(true);
+
+            if (inhabitant != null && inhabitant.InhabitantPrefab != null && instantiatedPrefab == null)
+            {
+                if (spawnPoint != null)
+                {
+                    instantiatedPrefab = Instantiate(
+                        inhabitant.InhabitantPrefab,
+                        spawnPoint.position,
+                        spawnPoint.rotation,
+                        spawnPoint
+                    );
+                    Debug.Log("Hi I just spawned!");
+                }
+                else
+                {
+                    Debug.LogWarning($"SpawnPoint not found on {gameObject.name}");
+                }
+            }
+        }
+    }
+}
